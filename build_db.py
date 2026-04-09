e7a3b310657cdfea6175995b2053fca19780accc
OanhKim-tmd <kimoanh16092005@gmail.com>
2026-04-09 14:13:32 +0700

    update

--- build_db.py
+++ build_db.py
@@ -5,7 +5,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import SupabaseVectorStore
 
-# 1. Thông tin cấu hình (Lấy trong mục Project Settings > API của Supabase)
+
 SUPABASE_URL = "https://qffwbeldaghwlyfnsnhc.supabase.co"
 SUPABASE_KEY = "sb_publishable_5EJ070mH65c4XUmBak0uag_ECktxoS-"
 GOOGLE_API_KEY = "AIzaSyDSPUd-Gf-_qnWLFgFz49vrmqFBroCLLBc"
@@ -13,12 +13,8 @@ GOOGLE_API_KEY = "AIzaSyDSPUd-Gf-_qnWLFgFz49vrmqFBroCLLBc"
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-# 2. Khởi tạo mô hình mã hóa (Embedding)
-# Dùng 768 chiều tương ứng với bảng SQL đã tạo ở Bước 1
-# Bỏ chữ 'models/' ở phía trước đi
-# Sửa lại dòng này trong file build_db.py
-# Cách này "ép" thư viện dùng đúng model mà không bị sai đường dẫn
-embeddings = GoogleGenerativeAIEmbeddings(model="embedding-001")
+
+embeddings = GoogleGenerativeAIEmbeddings(model="text-embedding-004")
 
 print("1. Đang đọc PDF...")
 loader = PyPDFDirectoryLoader("raw_data")
@@ -37,4 +33,4 @@ vector_store = SupabaseVectorStore.from_documents(
     query_name="match_documents",
 )
 
-print("\n✅ XONG! Kiến thức đã nằm gọn trên Supabase Cloud.")
+print("\n Kiến thức đã nằm gọn trên Supabase Cloud.")
